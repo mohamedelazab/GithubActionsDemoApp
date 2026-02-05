c743d54a1a5fca4bd7f6aaca0c9728c8f1bd834b
Mohamed Ali Elazab <melazab.c@tahakom.com>
2026-02-05 11:07:14 +0300

    update text

--- app/src/main/java/com/example/githubactionsdemoapp/MainActivity.kt
+++ app/src/main/java/com/example/githubactionsdemoapp/MainActivity.kt
@@ -21,7 +21,7 @@ class MainActivity : ComponentActivity() {
             GithubActionsDemoAppTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
-                        name = "Android",
+                        name = "Android World",
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
